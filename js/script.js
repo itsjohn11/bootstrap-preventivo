4a05c7bdc5376e8bebe1610699e95f8bdc1b0a04
its.john <giovannialesi87@icloud.com>
2025-03-12 17:34:40 +0100

    calcolo ore di lavoro

--- js/script.js
+++ js/script.js
@@ -1,53 +1,62 @@
 console.log("ciao");
 
-const oneHourBd = 20.50;
-const oneHoursFd = 15.30;
-const oneHoursPa = 33.60;
-
-
-// const backendDevelopment = 20.50;
-// const frontendDevelopment = 15.30;
-// const projectAnalysis = 33.60;
-
-// const backendDevelopment = parseInt(backendDevelopmentStr);
-// const frontendDevelopment = parseInt(frontendDevelopmentStr);
-// const projectAnalysis = parse(projectAnalysisStr)
 
 // let discount = 0;
 // if(YHDNU32, JANJC63, PWKCN25, SJDPO96, POCIE24) {
 //     discount = 35;
 // }
 
-const codeDiscount1 = YHDNU32;
-const codeDiscount2 = JANJC63;
-const codeDiscount3 = PWKCN25;
-const codeDiscount4 = SJDPO96;
-const codeDiscount5 = POCIE24;
-
-let discount = 0;
-if(YHDNU32){
-    discount = 35;
-}else if(JANJC63){
-    discount = 35;
-}else if(PWKCN25){
-    discount = 35;
-}else if(SJDPO96){
-    discount = 35;
-}else if(POCIE24){
-    discount = 35;
-}else (invalid){
-    disocunt = 0;
-}
 
 
 
-const requestQuote = document.getElementById("request-quote");
-const workSelect = document.getElementById("work");
+// let codeDiscount;
+//  YHDNU32;
+// console.log(codeDiscount);
+
+
+// const codeDiscount2 = JANJC63;
+// const codeDiscount3 = PWKCN25;
+// const codeDiscount4 = SJDPO96;
+// const codeDiscount5 = POCIE24;
+
+// let
+// if(YHDNU32){
+//     discount = 35;
+// }else if(JANJC63){
+//     discount = 35;
+// }else if(PWKCN25){
+//     discount = 35;
+// }else if(SJDPO96){
+//     discount = 35;
+// }else if(POCIE24){
+//     discount = 35;
+// }else if(invalid){
+//     discount = 0;
+// }
+const oneHourBd = 20.50;
+const oneHoursFd = 15.30;
+const oneHoursPa = 33.60; 
+console.log(oneHoursPa);
+
+const workingHoursBd = oneHourBd * 10;
+const workingHoursFd = oneHoursFd * 10;
+const workingHoursPa  = oneHoursPa * 10;
+
+
+if(oneHourBd === )
+
+// const requestQuote = document.getElementById("request-quote");
+// const workSelect = document.getElementById("work");
+// console.log(workSelect);
+
 
 requestQuote.addEventListener("submit", handleresquestQuote);
+console.log(requestQuote);
+
 
 function handleresquestQuote(event){
     event.preventDefault();
     const work = workSelect.value;
-    
+    console.log(work);
+
 }
